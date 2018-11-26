aee4e13c51bfd9566dd3dff0ad8b1ecb7fad4b9b
Adrian Mai <quocanh010@gmail.com>
2018-11-25 20:19:23 -0800

    Done

--- Maximum_Posterior_Probability.m
+++ Maximum_Posterior_Probability.m
@@ -4,15 +4,28 @@ Prior_2 = load('Prior_2.mat');
 alpha_m = load('Alpha.mat');
 alpha = alpha_m.alpha;
 %Extract data
-D1_BG = S.D1_BG;
-D2_BG = S.D2_BG;
-D3_BG = S.D3_BG;
-D4_BG = S.D4_BG;
+% D1_BG = S.D1_BG;
+% D2_BG = S.D2_BG;
+% D3_BG = S.D3_BG;
+% D4_BG = S.D4_BG;
+% 
+% D1_CT = S.D1_FG;
+% D2_CT = S.D2_FG;
+% D3_CT = S.D3_FG;
+% D4_CT = S.D4_FG;
 
-D1_CT = S.D1_FG;
-D2_CT = S.D2_FG;
-D3_CT = S.D3_FG;
-D4_CT = S.D4_FG;
+data_cell = cell(2,4);
+
+data_cell{1,1} = S.D1_BG;
+data_cell{1,2} = S.D2_BG;
+data_cell{1,3} = S.D3_BG;
+data_cell{1,4} = S.D4_BG;
+
+data_cell{2,1} = S.D1_FG;
+data_cell{2,2} = S.D2_FG;
+data_cell{2,3} = S.D3_FG;
+data_cell{2,4} = S.D4_FG;
+
 
 Prior_1_W0 = Prior_1.W0;
 Prior_1_m0_BG = Prior_1.mu0_BG;
@@ -22,44 +35,160 @@ Prior_2_W0 = Prior_2.W0;
 Prior_2_m0_BG = Prior_2.mu0_BG;
 Prior_2_m0_CT = Prior_2.mu0_FG;
 
-%Compute covariance matrix
-cov_D1_BG = cov(D1_BG);
-cov_D1_CT = cov(D1_CT);
-cov_D2_BG = cov(D2_BG);
-cov_D2_CT = cov(D2_CT);
-cov_D3_BG = cov(D3_BG);
-cov_D3_CT = cov(D3_CT);
-cov_D4_BG = cov(D4_BG);
-cov_D4_CT = cov(D4_CT);
-%Sample Mean
-me_1_BG = mean(D1_BG, 1);
-me_1_CT = mean(D1_CT, 1);
-
-%Part a
-%Covariance SIgma0 for 8 different value of alpha
-sigma0 = zeros(8, 64, 64);
-
-% Part
- for i = 1:8
-     sigma0(i,:,:) = diag(alpha(i) * Prior_1_W0);
- end
- % Calculate Prior
-Prior_Cheetah_1 = size(D1_CT, 1) / (size(D1_CT,1) + size(D1_BG, 1));
-Prior_Background_1 = size(D1_BG, 1) / (size(D1_CT,1) + size(D1_BG, 1));
 
 
-% Calculate mun for each class
 
-mun_1_BG = reshape(sigma0(1,:,:),64,64) * inv(reshape(sigma0(1,:,:),64,64) + (1/(size(D1_BG, 1))) *  cov_D1_BG)  * me_1_BG' + (1/size(D1_BG, 1)) * cov_D1_BG * inv(reshape(sigma0(1,:,:),64,64) + (1/size(D1_BG, 1)) * cov_D1_BG) * Prior_1_m0_BG';
-mun_1_CT = reshape(sigma0(1,:,:),64,64) * inv(reshape(sigma0(1,:,:),64,64) + (1/(size(D1_CT,1))) *  cov_D1_CT)  * me_1_CT' + (1/size(D1_CT,1)) * cov_D1_CT * inv(reshape(sigma0(1,:,:),64,64) + (1/(size(D1_CT,1))) * cov_D1_CT) * Prior_1_m0_CT';
 
-%Calcualte Cov_n
-cov_n_1 = 7  
 
- 
- 
- 
+img = imread('cheetah.bmp');
+img = im2double(img);
+%Vectorize 8-8 block with zig-zag patter
+% Padding to the image
+B = padarray(img, [7 7], 'symmetric','pre');
+m_Cheetah = zeros(255, 270);
+m_Background = zeros(255, 270);
+% Compute ans sliding window and decision matrix for 64 features
+B_masking_m_Cheetah = zeros(255, 270);
+B_masking_m_Background = zeros( 255, 270);
+ML_masking_m_Cheetah = zeros( 255, 270);
+ML_masking_m_Background = zeros( 255, 270);
+MAP_masking_m_Cheetah = zeros( 255, 270);
+MAP_masking_m_Background = zeros( 255, 270);
 
+B_e = zeros(9,1);
+MAP_e =  zeros(9,1);
+ML_e = zeros(9,1);
+
+masked_cheetah = imread('cheetah_mask.bmp');
+masked_cheetah = im2double(masked_cheetah);
+v = find(masked_cheetah);
+v1 = find(~masked_cheetah);
+
+% for al = 1:8
+%      sigma0 = diag(alpha(al) .* Prior_1_W0);
+%      mun_1_BG = sigma0 * inv(sigma0 + (1/(size(D1_BG, 1))) *  cov_D1_BG)  * me_1_BG' + (1/size(D1_BG, 1)) * cov_D1_BG * inv(sigma0 + (1/size(D1_BG, 1)) * cov_D1_BG) * Prior_1_m0_BG';
+%      mun_1_CT = sigma0 * inv(sigma0 + (1/(size(D1_CT,1))) *  cov_D1_CT)  * me_1_CT' + (1/size(D1_CT,1)) * cov_D1_CT * inv(sigma0 + (1/(size(D1_CT,1))) * cov_D1_CT) * Prior_1_m0_CT';
+%      cov_n_1_BG = sigma0 * inv(sigma0 + (1/(size(D1_BG, 1))) * cov_D1_BG) * (1/(size(D1_BG, 1))) * cov_D1_BG;
+%      cov_n_1_CT = sigma0 * inv(sigma0 + (1/(size(D1_CT, 1))) * cov_D1_CT) * (1/(size(D1_CT, 1))) * cov_D1_CT;
+%      B_cov_BG = cov_n_1_BG + cov_D1_BG;
+%      B_cov_CT = cov_n_1_CT + cov_D1_CT;
+%      for i = 1:size(img, 1)
+%          for j = 1:size(img, 2)
+%              temp_v = compute_dct_vector(B(i:i+7, j:j+7));
+%              temp_v = temp_v';
+%              B_masking_m_Cheetah(i, j) = (temp_v - mun_1_CT)' * inv(B_cov_CT) * (temp_v - mun_1_CT) + (log((2 * pi)^64 * det(B_cov_CT)) + Prior_Cheetah_1);
+%              B_masking_m_Background(i, j) = (temp_v - mun_1_BG)' * inv(B_cov_BG) * (temp_v - mun_1_BG) + (log((2 * pi)^64 * det(B_cov_BG)) + Prior_Background_1);
+%              MAP_masking_m_Cheetah(i, j) = (temp_v - mun_1_CT)' * inv(cov_D1_CT) * (temp_v - mun_1_CT) + (log((2 * pi)^64 * det(cov_D1_CT)) + Prior_Cheetah_1);
+%              MAP_masking_m_Background(i, j) = (temp_v - mun_1_BG)' * inv(cov_D1_BG) * (temp_v - mun_1_BG) + (log((2 * pi)^64 * det(cov_D1_BG)) + Prior_Background_1);
+%              ML_masking_m_Cheetah(i, j) = (temp_v - me_1_CT')' * inv(cov_D1_CT) * (temp_v - me_1_CT') + (log((2 * pi)^64 * det(cov_D1_CT)) + Prior_Cheetah_1);
+%              ML_masking_m_Background(i, j) = (temp_v - me_1_BG')' * inv(cov_D1_BG) * (temp_v - me_1_BG') + (log((2 * pi)^64 * det(cov_D1_BG)) + Prior_Background_1);
+%          end
+%      end
+%      
+%      n_img = ~(  B_masking_m_Background <= B_masking_m_Cheetah );
+%      %Calcualte error for 64 features
+%      flat_v_64 = n_img(:);
+%  
+%      %Calculating error
+%      P_Cheetah_g_Cheetah = nnz(flat_v_64(v) == 1) / numel(flat_v_64(v));
+%      P_Cheetah_g_Backgound = nnz(flat_v_64(v1) == 1) / numel(flat_v_64(v1));
+%      B_e(al,1) = P_Cheetah_g_Backgound * Prior_Background_1 + (1 - P_Cheetah_g_Cheetah) * Prior_Cheetah_1;
+%      
+%      n_img = ~(   MAP_masking_m_Background <= MAP_masking_m_Cheetah );
+%      %Calcualte error for 64 features
+%      flat_v_64 = n_img(:);
+%  
+%      %Calculating error
+%      P_Cheetah_g_Cheetah = nnz(flat_v_64(v) == 1) / numel(flat_v_64(v));
+%      P_Cheetah_g_Backgound = nnz(flat_v_64(v1) == 1) / numel(flat_v_64(v1));
+%      MAP_e(al,1) = P_Cheetah_g_Backgound * Prior_Background_1 + (1 - P_Cheetah_g_Cheetah) * Prior_Cheetah_1;
+%      
+%      n_img = ~(   ML_masking_m_Background <= ML_masking_m_Cheetah);
+%      %Calcualte error for 64 features
+%      flat_v_64 = n_img(:);
+%  
+%      %Calculating error
+%      P_Cheetah_g_Cheetah = nnz(flat_v_64(v) == 1) / numel(flat_v_64(v));
+%      P_Cheetah_g_Backgound = nnz(flat_v_64(v1) == 1) / numel(flat_v_64(v1));
+%      ML_e(al,1) = P_Cheetah_g_Backgound * Prior_Background_1 + (1 - P_Cheetah_g_Cheetah) * Prior_Cheetah_1;
+%      
+% 
+% 
+%      
+%      
+% end
+% % 64 features Gaussian Classifier
+for d = 1:4
+    cov_BG = cov(data_cell{1,d});
+    cov_CT = cov(data_cell{2,d});
+    me_BG = mean(data_cell{1,d},1);
+    me_CT = mean(data_cell{2,d},1);
+    Prior_Cheetah = size(data_cell{2,d}, 1) / (size(data_cell{1,d},1) + size(data_cell{2,d}, 1));
+    Prior_Background = size(data_cell{1,d}, 1) / (size(data_cell{1,d},1) + size(data_cell{2,d}, 1));
+
+     for al = 1:9
+         sigma0 = diag(alpha(al) .* Prior_1_W0);
+         mun_1_BG = sigma0 * ((sigma0 + (1/(size(data_cell{1,d}, 1))) *  cov_BG)  \ me_BG') + (1/size(data_cell{1,d}, 1)) * cov_BG * ((sigma0 + (1/size(data_cell{1,d}, 1)) * cov_BG) \ Prior_1_m0_BG');
+         mun_1_CT = sigma0 * ((sigma0 + (1/(size(data_cell{2,d},1))) *  cov_CT)  \ me_CT') + (1/size(data_cell{2,d},1)) * cov_CT * ((sigma0 + (1/(size(data_cell{2,d},1))) * cov_CT) \ Prior_1_m0_CT');
+         cov_n_1_BG = sigma0 * ((sigma0 + (1/(size(data_cell{1,d}, 1))) * cov_BG) \ ((1/(size(data_cell{1,d}, 1))) * cov_BG));
+         cov_n_1_CT = sigma0 * ((sigma0 + (1/(size(data_cell{2,d}, 1))) * cov_CT) \ ((1/(size(data_cell{2,d}, 1))) * cov_CT));
+         B_cov_BG = cov_n_1_BG + cov_BG;
+         B_cov_CT = cov_n_1_CT + cov_CT;
+         for i = 1:size(img, 1)
+             for j = 1:size(img, 2)
+                 temp_v = compute_dct_vector(B(i:i+7, j:j+7));
+                 temp_v = temp_v';
+                 B_masking_m_Cheetah(i, j) = (temp_v - mun_1_CT)' * ((B_cov_CT) \ (temp_v - mun_1_CT)) + (log((2 * pi)^64 * det(B_cov_CT)) + Prior_Cheetah);
+                 B_masking_m_Background(i, j) = (temp_v - mun_1_BG)' * ((B_cov_BG) \ (temp_v - mun_1_BG)) + (log((2 * pi)^64 * det(B_cov_BG)) + Prior_Background);
+                 MAP_masking_m_Cheetah(i, j) = (temp_v - mun_1_CT)' * ((cov_CT) \ (temp_v - mun_1_CT)) + (log((2 * pi)^64 * det(cov_CT)) + Prior_Cheetah);
+                 MAP_masking_m_Background(i, j) = (temp_v - mun_1_BG)' * ((cov_BG) \ (temp_v - mun_1_BG)) + (log((2 * pi)^64 * det(cov_BG)) + Prior_Background);
+                 ML_masking_m_Cheetah(i, j) = (temp_v - me_CT')' * ((cov_CT) \ (temp_v - me_CT')) + (log((2 * pi)^64 * det(cov_CT)) + Prior_Cheetah);
+                 ML_masking_m_Background(i, j) = (temp_v - me_BG')' * ((cov_BG) \ (temp_v - me_BG')) + (log((2 * pi)^64 * det(cov_BG)) + Prior_Background);
+             end
+         end
+
+         n_img = ~(  B_masking_m_Background <= B_masking_m_Cheetah );
+         %Calcualte error for 64 features
+         flat_v_64 = n_img(:);
+
+         %Calculating error
+         P_Cheetah_g_Cheetah = nnz(flat_v_64(v) == 1) / numel(flat_v_64(v));
+         P_Cheetah_g_Backgound = nnz(flat_v_64(v1) == 1) / numel(flat_v_64(v1));
+         B_e(al,1) = P_Cheetah_g_Backgound * Prior_Background + (1 - P_Cheetah_g_Cheetah) * Prior_Cheetah;
+
+         m_img = ~(   MAP_masking_m_Background <= MAP_masking_m_Cheetah );
+         %Calcualte error for 64 features
+         flat_v_64 = m_img(:);
+
+         %Calculating error
+         P_Cheetah_g_Cheetah = nnz(flat_v_64(v) == 1) / numel(flat_v_64(v));
+         P_Cheetah_g_Backgound = nnz(flat_v_64(v1) == 1) / numel(flat_v_64(v1));
+         MAP_e(al,1) = P_Cheetah_g_Backgound * Prior_Background + (1 - P_Cheetah_g_Cheetah) * Prior_Cheetah;
+
+         d_img = ~(   ML_masking_m_Background <= ML_masking_m_Cheetah);
+         %Calcualte error for 64 features
+         flat_v_64 = d_img(:);
+
+         %Calculating error
+         P_Cheetah_g_Cheetah = nnz(flat_v_64(v) == 1) / numel(flat_v_64(v));
+         P_Cheetah_g_Backgound = nnz(flat_v_64(v1) == 1) / numel(flat_v_64(v1));
+         ML_e(al,1) = P_Cheetah_g_Backgound * Prior_Background + (1 - P_Cheetah_g_Cheetah) * Prior_Cheetah;
+
+     end
+figure;
+plot(log10(alpha), B_e','o-','linewidth',3,'markersize',10,'markerfacecolor','g')
+hold on;
+plot(log10(alpha), MAP_e','o-','linewidth',3,'markersize',10,'markerfacecolor','g')
+plot(log10(alpha), ML_e','o-','linewidth',3,'markersize',10,'markerfacecolor','g')
+legend('Bayes','MAP','ML');
+xlabel('log10(alpha)') ;
+ylabel('Probability of error') ;
+title(['D',num2str(d)]);
+hold off;
+
+
+end
+% 64 features Gaussian Clas
  
  
 
@@ -95,8 +224,6 @@ cov_n_1 = 7
 % colormap(gray(256));
 % title(['64 features masked '])
 % axis equal;
-
-
 
 
 
